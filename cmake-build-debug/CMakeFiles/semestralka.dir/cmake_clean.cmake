54b20559f43ee83bd1ae331cdf01eff7b1ae65b9
maria710 <mariakuruczova7@gmail.com>
2022-11-15 16:51:37 +0100

    M01: init

---
+++ cmake-build-debug/CMakeFiles/semestralka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/semestralka.dir/main.cpp.o"
+  "semestralka"
+  "semestralka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/semestralka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
